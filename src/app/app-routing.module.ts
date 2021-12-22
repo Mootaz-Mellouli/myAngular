40fba341999ea7165d1687e4f5424040f1224ff1
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2021-12-22 11:27:17 +0100

    home + notfound component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 import {  RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
 
 
 const routes : Routes = [
   {path: 'about', component: AboutComponent},
-  {path:'contact', component:ContactComponent}
+  {path:'contact', component:ContactComponent},
+  {path:'',component:HomeComponent}
 ] ;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    NotfoundComponent
+  ]
 })
 export class AppRoutingModule { }
