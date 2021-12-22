e8306e2dec3fe8ad1f730e678ccd36d932182256
Mootaz-Mellouli <mootazmellouli@hotmail.com>
2021-12-22 09:10:18 +0100

    routing system + creating about and contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {  RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
+const routes : Routes = [
+  {path: 'about', component: AboutComponent},
+  {path:'contact', component:ContactComponent}
+] ;
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
